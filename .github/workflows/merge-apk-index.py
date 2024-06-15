6973dc4062d65d844c7ab683cf3fba408d6b01c5
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2024-06-15 15:15:33 +0200

    test

--- .github/workflows/merge-apk-index.py
+++ .github/workflows/merge-apk-index.py
@@ -4,19 +4,34 @@ import json
 # based on https://stackoverflow.com/a/52879570
 # todo: see how this can be done with proper melange tooling
 
-apk_index_files = glob.glob("**/APKINDEX.json", recursive=True)
+apk_index_files_json = glob.glob("**/APKINDEX.json", recursive=True)
+output_list_json = []
+
+for f in apk_index_files_json:
+    with open(f, "rb") as infile:
+        output_list_json.append(json.load(infile))
+
+merged_apk_index_json = {}
+all_packages = []
+for json_file in output_list_json:
+   all_packages.extend(json_file['Packages'])
+
+merged_apk_index_json['Packages'] = all_packages
+
+textfile_merged_json = open('repo/x86_64/APKINDEX.json', 'w')
+textfile_merged_json.write(json.dumps(merged_apk_index_json))
+
+
+
+
+apk_index_files = glob.glob("**/APKINDEX", recursive=True)
 output_list = []
 
 for f in apk_index_files:
     with open(f, "rb") as infile:
-        output_list.append(json.load(infile))
+        output_list.append(infile)
 
-merged_apk_index = {}
-all_packages = []
-for json_file in output_list:
-   all_packages.extend(json_file['Packages'])
+merged = '\n'.join(output_list)
 
-merged_apk_index['Packages'] = all_packages
-
-textfile_merged = open('repo/x86_64/APKINDEX.json', 'w')
-textfile_merged.write(json.dumps(merged_apk_index))
+textfile_merged = open('apk-index-temp/APKINDEX', 'w')
+textfile_merged.write(merged)
