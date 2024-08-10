5085a1cd871c4af2bd4a3c89d1976b29420e3090
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2024-08-10 20:28:04 +0200

    update sha

--- update.py
+++ update.py
@@ -1,6 +1,8 @@
 import sys
 import requests
 import json
+import urllib.request
+import hashlib
 
 from yaml import load, dump
 try:
@@ -13,6 +15,8 @@ if __name__ == '__main__':
     file_content = ''
     old_version = ''
     new_version = ''
+    old_sha = ''
+    new_sha = ''
     with open(f'manifests/{package}.yaml', 'r') as f:
         file_content = str(f.read())
     with open(f'manifests/{package}.yaml', 'r') as f:
@@ -32,12 +36,30 @@ if __name__ == '__main__':
                 latest_versions = json.loads(response.content)
                 print(latest_versions['latest_version'])
                 new_version = latest_versions['latest_version']
+
+                download_coordinates = manifest['pipeline'][0]['with']
+                if not download_coordinates:
+                    raise('error')
+                uri_template: str = download_coordinates['uri']
+                old_sha = download_coordinates['expected-sha256']
+
+                uri = uri_template.replace('${{package.version}}', new_version)
+                (path, response) = urllib.request.urlretrieve(uri)
+                print(uri)
+                print(path)
+                with open(path, 'rb', buffering=0) as f:
+                    new_sha = hashlib.file_digest(f, 'sha256').hexdigest()
+                    print(old_sha)
+                    print(new_sha)
+
             if 'github' in update:
                 github = update['github']
                 identifier = github['identifier']
                 strip_prefix = github['strip-prefix']
                 tag_filter = github['tag-filter']
 
-    new_file_content = file_content.replace(old_version, new_version)
+    new_file_content = file_content\
+        .replace(old_version, new_version)\
+        .replace(old_sha, new_sha)
     with open(f'manifests/{package}.yaml', 'w') as f:
         f.write(new_file_content)
