2496103afbfdbc7d4a2403c8a16594e15d42ce13
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2024-08-11 21:01:10 +0200

    wip

--- update.py
+++ update.py
@@ -3,6 +3,9 @@ import requests
 import json
 import urllib.request
 import hashlib
+import tempfile
+
+from git import Repo
 
 from yaml import load, dump
 try:
@@ -37,26 +40,39 @@ if __name__ == '__main__':
                 print(latest_versions['latest_version'])
                 new_version = latest_versions['latest_version']
 
-                download_coordinates = manifest['pipeline'][0]['with']
-                if not download_coordinates:
-                    raise('error')
-                uri_template: str = download_coordinates['uri']
-                old_sha = download_coordinates['expected-sha256']
+                fetch_step = manifest['pipeline'][0]
+                if fetch_step['uses'] == 'fetch':
+                    download_coordinates = fetch_step['with']
+                    if not download_coordinates:
+                        raise('error')
+                    uri_template: str = download_coordinates['uri']
+                    old_sha = download_coordinates['expected-sha256']
 
-                uri = uri_template.replace('${{package.version}}', new_version)
-                (path, response) = urllib.request.urlretrieve(uri)
-                print(uri)
-                print(path)
-                with open(path, 'rb', buffering=0) as f:
-                    new_sha = hashlib.file_digest(f, 'sha256').hexdigest()
-                    print(old_sha)
-                    print(new_sha)
+                    uri = uri_template.replace('${{package.version}}', new_version)
+                    try:
+                        (path, response) = urllib.request.urlretrieve(uri)
+                        print(uri)
+                        print(path)
+                        with open(path, 'rb', buffering=0) as f:
+                            new_sha = hashlib.file_digest(f, 'sha256').hexdigest()
+                            print(old_sha)
+                            print(new_sha)
+                    except:
+                        print('error')
+                        sys.exit(0)
+
+                if fetch_step['uses'] == 'git-checkout':
+                    git_repo_coordinates = fetch_step['with']
+                    # with tempfile.TemporaryDirectory() as tmpdirname:
+
+                    #     repo = Repo.clone_from(git_repo_coordinates.repository, f'{tmpdirname}/repo')
+                    #     repo.br
 
             if 'github' in update:
                 github = update['github']
-                identifier = github['identifier']
-                strip_prefix = github['strip-prefix']
-                tag_filter = github['tag-filter']
+                # identifier = github['identifier']
+                # strip_prefix = github['strip-prefix']
+                # tag_filter = github['tag-filter']
 
     new_file_content = file_content\
         .replace(old_version, new_version)\
