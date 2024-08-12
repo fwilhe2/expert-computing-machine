a99bdfd9c5a9987aeb19456ac00f63447e31c6c6
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2024-08-12 20:42:05 +0200

    sort sbom

--- .github/workflows/md-sbom.py
+++ .github/workflows/md-sbom.py
@@ -4,14 +4,21 @@ from pathlib import Path
 
 sbom_files = glob.glob("rootfs/var/lib/db/sbom/*.json")
 
-print("# SBOM")
-
-print("| Package Name | Version | Declared Licenses |")
-print("| --- | --- | --- |")
+package_list = []
 
 for f in sbom_files:
     with open(f, 'r') as ff:
         sbom = json.load(ff)
         packages = sbom['packages']
         for p in packages:
-            print(f"| {p['name']} | {p['versionInfo']} | {p['licenseDeclared']} |")
+            package_list.append(f"| {p['name']} | {p['versionInfo']} | {p['licenseDeclared']} |")
+
+package_list.sort()
+
+print("# SBOM")
+
+print("| Package Name | Version | Declared Licenses |")
+print("| --- | --- | --- |")
+
+for p in package_list:
+    print(p)
