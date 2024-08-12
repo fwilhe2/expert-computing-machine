502ea374631f7e9b1c355f191c06e1414953a174
Florian Wilhelm <52838694+fwilhe2@users.noreply.github.com>
2024-08-12 22:12:27 +0200

    docker runner

--- build-package.sh
+++ build-package.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 PACKAGE=$1
 
-melange build --arch x86_64 --keyring-append https://packages.wolfi.dev/os/wolfi-signing.rsa.pub --repository-append https://packages.wolfi.dev/os manifests/$PACKAGE.yaml
+melange build --arch x86_64 --keyring-append https://packages.wolfi.dev/os/wolfi-signing.rsa.pub --repository-append https://packages.wolfi.dev/os --runner docker manifests/$PACKAGE.yaml
 
 pushd packages/x86_64
 tar xzf APKINDEX.tar.gz
