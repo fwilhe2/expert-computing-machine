f8cb0e6249cc41fb821c326838fb70eb90ff6041
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2024-08-14 19:41:41 +0200

    Build with qemu runner

---
+++ build-package-qemu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o errexit
+
+PACKAGE=$1
+
+melange build --arch x86_64 --keyring-append https://packages.wolfi.dev/os/wolfi-signing.rsa.pub --repository-append https://packages.wolfi.dev/os --runner qemu manifests/$PACKAGE.yaml
+
+pushd packages/x86_64
+tar xzf APKINDEX.tar.gz
+mv APKINDEX $PACKAGE-APKINDEX
+
+popd
