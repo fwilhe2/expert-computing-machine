984b29cbf9524f8f6ad04a6bc17a4c260de9a1f8
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2024-09-06 22:41:12 +0200

    test

--- build-package.sh
+++ build-package.sh
@@ -8,6 +8,12 @@ melange build --arch x86_64,arm64 --keyring-append https://packages.wolfi.dev/os
 
 pushd packages/x86_64
 tar xzf APKINDEX.tar.gz
-mv APKINDEX $PACKAGE-APKINDEX
-
+mv APKINDEX $PACKAGE-APKINDEX-x86_64
 popd
+
+
+pushd packages/aarch64
+tar xzf APKINDEX.tar.gz
+mv APKINDEX $PACKAGE-APKINDEX-aarch64
+popd
+
--- make-apk-repo.sh
+++ make-apk-repo.sh
@@ -2,8 +2,10 @@
 
 set -o errexit
 
+set -x
+
 mkdir -p repo/x86_64
-find . -name '*.apk' -exec cp {} repo/x86_64 \;
+find . -name '*.apk' -exec grep x86_64 | cp {} repo/x86_64 \;
 find . -name 'APKINDEX.tar.gz' -exec tar xf {} \;
 python3 .github/workflows/merge-apk-index.py
 touch repo/x86_64/DESCRIPTION
@@ -11,4 +13,16 @@ cat repo/x86_64/APKINDEX
 pushd repo/x86_64
 tar czf APKINDEX.tar.gz APKINDEX DESCRIPTION
 popd
+
+mkdir -p repo/aarch64
+find . -name '*.apk' -exec grep aarch64 | cp {} repo/aarch64 \;
+find . -name 'APKINDEX.tar.gz' -exec tar xf {} \;
+python3 .github/workflows/merge-apk-index.py
+touch repo/aarch64/DESCRIPTION
+cat repo/aarch64/APKINDEX
+pushd repo/aarch64
+tar czf APKINDEX.tar.gz APKINDEX DESCRIPTION
+popd
+
+
 find repo
