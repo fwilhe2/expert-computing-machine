0cedc7b32487355a300b2f4d9837e3b4db021271
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2024-09-08 08:30:33 +0200

    test

--- make-apk-repo.sh
+++ make-apk-repo.sh
@@ -7,17 +7,20 @@ set -x
 mkdir -p repo/x86_64
 find . -name '*.apk' -exec sh -c "grep x86_64 | cp {} repo/x86_64" \;
 find . -name 'APKINDEX.tar.gz' -exec tar xf {} \;
+
+mkdir -p repo/aarch64
+find . -name '*.apk' -exec sh -c "grep aarch64 | cp {} repo/aarch64" \;
+find . -name 'APKINDEX.tar.gz' -exec tar xf {} \;
+
 python3 .github/workflows/merge-apk-index.py
+
 touch repo/x86_64/DESCRIPTION
 cat repo/x86_64/APKINDEX
 pushd repo/x86_64
 tar czf APKINDEX.tar.gz APKINDEX DESCRIPTION
 popd
 
-mkdir -p repo/aarch64
-find . -name '*.apk' -exec sh -c "grep aarch64 | cp {} repo/aarch64" \;
-find . -name 'APKINDEX.tar.gz' -exec tar xf {} \;
-python3 .github/workflows/merge-apk-index.py
+
 touch repo/aarch64/DESCRIPTION
 cat repo/aarch64/APKINDEX
 pushd repo/aarch64
