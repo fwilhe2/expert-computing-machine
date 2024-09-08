aab37433c690021977469403c4f78064d1c384a1
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2024-09-08 21:30:09 +0200

    test

--- make-rootfs.sh
+++ make-rootfs.sh
@@ -6,9 +6,9 @@ set -x
 apko --log-level debug --build-arch amd64 build-minirootfs os.yaml rootfs-amd64.tgz
 mkdir -p rootfs-amd64
 sudo tar xzf rootfs-amd64.tgz -C rootfs-amd64
-sudo /sbin/mkfs.ext4 -L root -d ./rootfs/ rootfs-amd64.ext4 2G
+sudo /sbin/mkfs.ext4 -L root -d ./rootfs-amd64/ rootfs-amd64.ext4 2G
 
 apko --log-level debug --build-arch aarch64 build-minirootfs os.yaml rootfs-aarch64.tgz
 mkdir -p rootfs-aarch64
 sudo tar xzf rootfs-aarch64.tgz -C rootfs-aarch64
-sudo /sbin/mkfs.ext4 -L root -d ./rootfs/ rootfs-aarch64.ext4 2G
+sudo /sbin/mkfs.ext4 -L root -d ./rootfs-aarch64/ rootfs-aarch64.ext4 2G
